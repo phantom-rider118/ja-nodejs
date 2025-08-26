6e5dd7563e1eae7923df7a56c592961142806bc9
Nicolle Benavides <nicolle.benavides-milla@capgemini.com>
2025-08-26 16:18:26 +0200

    messaggio personalizzato

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,3 +3,7 @@ const greet = require('../app');
 test('greets the user by name', () => {
   expect(greet('DevOps')).toBe('Hello, DevOps!');
 });
+
+test('cacazione', () => {
+  expect(nicolle()).toBe('Cacati addosso!');
+});  
--- app.js
+++ app.js
@@ -2,4 +2,8 @@ function greet(name) {
   return `Hello, ${name}!`;
 }
 
+function nicolle(){
+  return "Cacati addosso!";
+}
+
 module.exports = greet;
